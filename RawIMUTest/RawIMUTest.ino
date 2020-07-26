7c31eea42836eca221adf92673bc147f0e7f65fa
NeilRajah <neil.balaskandarajah@gmail.com>
2020-07-25 22:30:02 -0400

    Removed resistors, IMU working
    - going to work off MPU example to get IMU data
      - just need yaw
      - going to strip example code down to just IMU
      - need to avoid using pin 2 for interrupts (already used by encoder)

---
+++ RawIMUTest/RawIMUTest.ino
@@ -0,0 +1,44 @@
+/**
+ * IMUTest
+ * Author: Neil Balaskandarajah
+ * Created on: 07/25/2020
+ * Testing out the IMU
+ */
+#include<Wire.h>
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+void setup(){
+  Serial.begin(9600);
+  Serial.println("hello");
+  Wire.begin();
+  Serial.println("Wire.begin();");
+  Wire.beginTransmission(MPU_addr);
+  Serial.println("Wire.beginTransmission(MPU_addr);");
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Serial.println("Wire.write(0x6B);");
+  Wire.write(1);     // set to zero (wakes up the MPU-6050)
+  Serial.println("Wire.write(1);");
+  Wire.endTransmission();
+  Serial.println("Wire.endTransmission();");
+}
+void loop(){
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+  delay(20);
+}
